81775a5c60287082b8fd0356ef9bb24547599e0d
AngelyneBC <angelynealvarez00@gmail.com>
2026-07-18 03:29:10 -0500

    userEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 import './App.css';
 import Panel from './components/Panel/Panel';
